e6d61ed6c2974d7949af781a1e64ddca96d104a7
ykiriya <77214610+ykiriya@users.noreply.github.com>
2022-06-16 19:46:28 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    custom.fo1()
+    custom.fo5()
+    custom.fo3()
+})
+input.onButtonPressed(Button.B, function () {
+    custom.foo()
+    custom.foo()
+    custom.foo()
+})
 basic.forever(function () {
-    cuteBot.motors(-5, 30)
+    if (cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters) > 15) {
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+    } else {
+        cuteBot.closeheadlights()
+    }
 })

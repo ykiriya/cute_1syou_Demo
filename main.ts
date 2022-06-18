1d8d16ab631dcaeb540167d8b59cb816789cd349
ykiriya <77214610+ykiriya@users.noreply.github.com>
2022-06-18 19:54:00 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,15 +3,31 @@ input.onButtonPressed(Button.A, function () {
     custom.fo5()
     custom.fo3()
 })
+function zensin () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+        if (cuteBot.kyori2()) {
+            cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+            if (cuteBot.kyori()) {
+                cuteBot.motors(30, -29)
+            } else {
+                cuteBot.motors(30, 10)
+            }
+        } else {
+            cuteBot.motors(30, 30)
+            cuteBot.closeheadlights()
+        }
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.stopcar()
+        cuteBot.motors(-30, -30)
+    } else {
+    	
+    }
+}
 input.onButtonPressed(Button.B, function () {
     custom.foo()
     custom.foo()
     custom.foo()
 })
 basic.forever(function () {
-    if (cuteBot.kyori()) {
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
-    } else {
-        cuteBot.closeheadlights()
-    }
+	
 })
